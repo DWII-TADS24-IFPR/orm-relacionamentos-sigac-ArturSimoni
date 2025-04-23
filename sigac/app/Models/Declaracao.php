b6ea015b8b57b850e2c26f63305f13f5192014fd
Luiza Caponi <caponiluiza@gmail.com>
2025-04-23 08:51:13 -0300

    bug do php não dar commit corrigido

---
+++ sigac/app/Models/Declaracao.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Declaracao extends Model
+{
+    protected $table = "";
+    protected $fillable = [ 
+        
+    ];
+
+    use HasFactory;
+}
